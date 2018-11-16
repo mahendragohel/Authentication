c971621128250baa3b986a2146c4f1eb1b8c53a0
maddy1690 <mahendra.gohel@outlook.com>
2018-11-16 16:21:44 +0530

    signup and registration method for admin in auth controller

--- src/main/java/com/auth/controller/AuthController.java
+++ src/main/java/com/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -99,5 +100,33 @@ public class AuthController {
         
         ResponseBean response = new ResponseBean(new JwtAuthenticationResponse(jwt,user.getRoles()));
         return new ResponseEntity<ResponseBean>(response,HttpStatus.OK);
+    }
+	
+	@PostMapping("/admin/registration")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ResponseBean> registerAdmin(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+        User result = userRepository.save(user);
+		ResponseBean response = new ResponseBean(new ApiResponse(true, "Admin Register successfully!"));
+		return new ResponseEntity<ResponseBean>(response,HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/auth/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.auth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.auth.payload.UserIdentityAvailability;
+import com.auth.payload.UserSummary;
+import com.auth.repository.UserRepository;
+import com.auth.security.CurrentUser;
+import com.auth.security.UserPrincipal;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER') or hasRole('ROLE_ADMIN')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+}
